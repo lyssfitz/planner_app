e8644318239e01af936feec95ec697a8a1107dcc
Lyss <lyssfitz@outlook.com>
2020-02-08 19:13:16 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -3,24 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Saturday</h1>
+			<div className="big3">
+				<h2>DAILY BIG 3 List your three most important tasks.</h2>
+				<p>Wash clothes</p>
+				<p>Workout</p>
+				<p>Cook dinner</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
